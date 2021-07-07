a7f0845e56a849492f321b2b76fa54f24dd2189d
RatihEl <ratiheldina1234@gmail.com>
2021-07-07 08:35:50 +0700

    first commit

---
+++ lib/tampilku.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class WebViewInFlutter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: 'https://kucingtekno.com',
+      hidden: true,
+      appBar: AppBar(title: Text("Kucing Tekno Com ")),
+    );
+  }
+}
